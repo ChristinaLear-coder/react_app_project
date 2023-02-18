3f6753e3055591f24e53f91f44da4303a58cc6c1
ChristinaLear-coder <115825078+ChristinaLear-coder@users.noreply.github.com>
2023-02-18 20:39:13 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import React from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App">React Weather App</div>;
+export default function App() {
+  return (
+    <div className="App">
+      <h1> React Weather App</h1>
+      <footer>
+        Coded by Christina Lear
+        <a
+          href="https://github.com/ChristinaLear-coder/react_app_project"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
-
-export default App;
